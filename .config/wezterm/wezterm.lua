31f4c4a90f74a750ac3007940e59de49ee84531f
Aman Kumar <akumar999022@gmail.com>
2024-02-22 12:30:39 +0530

    Change colors to cyan like colors

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,10 +10,21 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.color_scheme = "Catppuccin Mocha"
+-- config.color_scheme = "Catppuccin Mocha"
 config.colors = {
 	foreground = "#fff",
 	background = "#1C1C1C",
+
+	-- foreground = "#ecdef4",
+	-- background = "#010c18",
+
+	cursor_bg = "#38ff9d",
+	cursor_border = "#38ff9d",
+	cursor_fg = "#000000",
+	selection_bg = "#38ff9c",
+	selection_fg = "#000000",
+	ansi = { "#0b3b61", "#ff3a3a", "#52ffd0", "#fff383", "#1376f9", "#c792ea", "#ff5ed4", "#16fda2" },
+	brights = { "#63686d", "#ff54b0", "#74ffd8", "#fcf5ae", "#388eff", "#ae81ff", "#ff6ad7", "#60fbbf" },
 }
 
 config.font = wezterm.font("Iosevka Nerd Font", { italic = false })
